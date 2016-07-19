0aec7018e422c2e200d7434b0077c73f6bf83a14
Josh English <josh@joshenglish.com>
2016-07-19 15:43:05 -0500

    Making adjustments to add pure-render-mxin for react

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 export default class TextInput extends Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
   render() {
     return <input className="edit" autoFocus={true} type="text" />
   }
