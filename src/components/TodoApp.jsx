d356d784bbe228a626ff253117ca936e11f80e03
Josh English <josh@joshenglish.com>
2016-07-20 14:15:19 -0500

    Added TodoHeader with header information

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import TodoList from './TodoList'
+import TodoHeader from './TodoHeader'
 
 export default class TodoApp extends Component {
   render() {
     return <div>
       <section className="todoapp">
+        <TodoHeader />
         <TodoList todos={this.props.todos} filter={this.props.filter} />
       </section>
     </div>
---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+export default class TextInput extends Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
+  _handleKeyPress(e) {
+    if (e.key === 'Enter' && this.refs.addTodoInput.value !== '') {
+      const itemText = this.refs.addTodoInput.value
+      this.refs.addTodoInput.value = ''
+      return this.props.addItem(itemText)
+    }
+  }
+  render() {
+    return <header className="header">
+      <h1>todos</h1>
+      <input className="new-todo"
+             ref="addTodoInput"
+             autofocus
+             autoComplete="off"
+             placeholder="What needs to be done?"
+             onKeyPress={this._handleKeyPress.bind(this)}
+    </header>
+  }
+}
