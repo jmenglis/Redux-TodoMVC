492578e9cfdc3cd0f94ec27a5fe8a8aa11e5fc72
Josh English <josh@joshenglish.com>
2016-07-20 14:42:33 -0500

    Adding Footer and TodoTools and ensuring Header is properly completed

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import TodoList from './TodoList'
 import TodoHeader from './TodoHeader'
+import TodoTools from './TodoTools'
+import Footer from './Footer'
 
 export default class TodoApp extends Component {
+  getNbActiveItems() {
+    if (this.props.todos) {
+      const activeitems = this.props.todos.filter(
+        (item) => item.get('status') === 'active'
+      )
+      return activeItems.size
+    }
+    return 0
+  }
   render() {
     return <div>
       <section className="todoapp">
         <TodoHeader />
         <TodoList todos={this.props.todos} filter={this.props.filter} />
+        <TodoTools fitler={this.props.filter} nbActiveItems={this.getNbActiveItems()} />
       </section>
+      <Footer />
     </div>
   }
 }
--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
-export default class TextInput extends Component {
+export default class TodoHeader extends Component {
   constructor(props) {
     super(props)
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
