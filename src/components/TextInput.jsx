8d50694dd1e866d84b6ef057ddde1ab5c8ec843c
Josh English <josh@joshenglish.com>
2016-07-20 13:45:35 -0500

    Added changes to React Component

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import classNames from 'classnames'
 
 export default class TextInput extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      value: props.text
+    }
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
   }
+  cancelEditing() {
+    this.setState({
+      value: this.props.text
+    })
+    return this.props.cancelEditing(this.props.itemId)
+  }
+  _handleKeyDown(e) {
+    switch (e.key) {
+      case 'Enter':
+        return this.props.doneEditing(this.props.itemId, this.state.value)
+      case 'Escape':
+        return this.cancelEditing()
+    }
+  }
+  _handleOnBlur(e) {
+    return this.cancelEditing();
+  }
+  _handleOnChange(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
   render() {
-    return <input className="edit" autoFocus={true} type="text" />
+    return <input className="edit"
+                  autoFocus={true}
+                  type="text"
+                  value={this.state.value}
+                  onChange={this._handleOnChange.bind(this)}
+                  ref="itemInput"
+                  onKeyDown={this._handleKeyDown.bind(this)}
+                  onBlur={this._handleOnBlur.bind(this)}
+    />
   }
 }
