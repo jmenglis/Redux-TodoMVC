683f44bfc4831294211d7226a46fbee50055da8b
Josh English <josh@joshenglish.com>
2016-07-20 14:02:16 -0500

    Added changes from TextInput to pass down props of functions from Todoitem and TodoList.  Wrote test for TextInput_spec

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -32,7 +32,11 @@ export default class TodoItem extends Component {
         >
         </button>
       </div>
-      <TextInput />
+      <TextInput text={this.props.text}
+                 itemId={this.props.id}
+                 cancelEditing={this.props.cancelEditing}
+                 doneEditing={this.props.doneEditing}
+      />
     </li>
   }
 }
